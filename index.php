3306b968bae609b01167d2cfaa3c79039e82acb0
Konstantin Protzen <code@kondev.de>
2023-10-21 16:58:08 +0200

    🎉 Init

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CloStat</title>
+    <link rel="stylesheet" href="style.css" />
+    <script defer src="/res/icons/friconix.js"></script>
+  </head>
+  <body>
+    <div class="entry-list"></div>
+    <div class="clothing-list">
+      <i class="fi-xnsuxl-tshirt-solid"></i>
+    </div>
+    <div class="statistics">
+      <i class="fi-xnsuxl-signal-solid"></i>
+    </div>
+    <div class="add">
+      <i class="fi-cwsuxl-plus-solid"></i>
+    </div>
+  </body>
+</html>
